8ea3eea465781ed45b5e93d9887c83861b2fcc77
Carl Clegg <carl.clegg@connectchildcare.com>
2018-02-22 16:32:47 +0000

    Added icon, basic PoC of proper addon over the observatory only one

--- background.js
+++ background.js
@@ -4,17 +4,47 @@ if (chrome)
 
 // Add the context menu
 browser.contextMenus.create({
-    id: "open-in-observatory",
-    title: "Open in Mozilla Observatory"
+    id: "open-in-obervatory",
+    title: "Mozilla Observatory"
+});
+browser.contextMenus.create({
+  id: "separator-1",
+  type: "separator"
+});
+
+browser.contextMenus.create({
+    id: "open-in-ssllabs",
+    title: "SSL Labs"
+});
+
+browser.contextMenus.create({
+    id: "open-in-security-headers",
+    title: "SecurityHeaders"
 });
 
 // Handle the click
 browser.contextMenus.onClicked.addListener(function(info, tab) {
     // Get current domain
     // open url
+    var base = "https://observatory.mozilla.org/analyze.html?host="
+
+    switch(info.menuItemId) {
+        case "open-in-ssllabs":
+            base = "https://www.ssllabs.com/ssltest/analyze?d=";
+            break;
+        case "open-in-observatory":
+            base = "https://observatory.mozilla.org/analyze.html?host=";
+            break;
+        case "open-in-security-headers":
+            base = "https://securityheaders.io/?followRedirects=on&hide=on&q=";
+            break;
+        default:
+            break;
+    }
+
     var domain = info.pageUrl.split("/")[2]
     console.log(domain);
     browser.tabs.create({
-        url: "https://observatory.mozilla.org/analyze.html?host=" + domain
+        url: base + domain
     })
 })
